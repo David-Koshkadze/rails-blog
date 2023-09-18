6b172cb592636b150e0b4b0fdc3a510e2a3e436d
David-Koshkadze <rootUS3rr@pm.me>
2023-09-18 16:17:32 +0400

    CRUD/update, delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,18 +7,42 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])  
   end
 
+  # new to get the form page and pass @article instance variable which contains article info
   def new
     @article = Article.new
   end
 
+  # creating new article, POST request
   def create 
-    @article = Article.new(title: "...", body: "")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  # get the edit page, find specific article and pass this a instance variable
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
