0ac0a0f3854fd1e97ccb82689d2ffccc3969f86a
David-Koshkadze <rootUS3rr@pm.me>
2023-09-19 11:47:22 +0400

    destroy comment and associated comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  # has_many relationship with comments. 
+  # When deleted remove associated objects in that case associated comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
