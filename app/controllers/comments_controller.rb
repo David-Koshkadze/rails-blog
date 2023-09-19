f047857a12337ea05d4ec816a7edd66fcfde9085
David-Koshkadze <rootUS3rr@pm.me>
2023-09-19 11:11:44 +0400

    implement status concern

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:comment_author, :body)   
+    params.require(:comment).permit(:comment_author, :body, :status)   
   end
 end
